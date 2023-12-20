c4d7124a24bff875548c205dd7e36a9c1ce85689
calebgl <57515964+calebgl@users.noreply.github.com>
2023-12-19 22:11:33 -0600

    add logic to fetch all feeds concurrently

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/calebgl/rss-aggregator/internal/database"
 	"github.com/go-chi/chi/v5"
@@ -35,9 +36,12 @@ func main() {
 		log.Fatal("Can't connect to database")
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
